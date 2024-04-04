49458b82a5b0805d1108f5086e6327f2b74e83e1
Li Fei <56876213+lifeiny@users.noreply.github.com>
2024-04-04 13:20:12 +0800

    Update config.ts
    change link

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SITE = {
 	keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
 	icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
 	pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
-	homePage: 'https://weekly.tw93.fun',
+	homePage: 'https://lifeiny.github.io/',
 	blogPage: 'https://tw93.fun/',
 	twitterId: 'HiTw93',
 	githubId: 'tw93',
